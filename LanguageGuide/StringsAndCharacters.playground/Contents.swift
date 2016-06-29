ce102923a2c764d449653721f030aadbc2b6387e
= <=>
2016-06-29 19:22:54 +0100

    Strings and Characters - Unicode, Counting Characters

--- LanguageGuide/StringsAndCharacters.playground/Contents.swift
+++ LanguageGuide/StringsAndCharacters.playground/Contents.swift
@@ -57,3 +57,43 @@ welcome.append(exclamationMark)
 
 let multiplier = 3
 let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+
+// -------------------------------------------------------------------------------
+// Unicode
+// -------------------------------------------------------------------------------
+
+// Special Characters in String Literals
+
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+let dollarSign = "\u{24}"
+let blackHeart = "\u{2665}"
+let sparklingHeart = "\u{1f496}"
+
+// Extended Grapheme Clusters
+
+let eAcute = "\u{e9}"
+let combinedEAcute = "\u{65}\u{301}"
+
+let precomposed: Character = "\u{D55C}"
+let decomposed: Character = "\u{1112}\u{1161}\u{11AB}"
+
+let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
+
+// -------------------------------------------------------------------------------
+// Counting Characters
+// -------------------------------------------------------------------------------
+
+let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🐧, Dromedary 🐪"
+print("unusualMenagerie has \(unusualMenagerie.characters.count) characters")
+
+var word = "cafe"
+print("the number of characters in \(word) is \(word.characters.count)")
+
+word += "\u{301}"
+
+print("the number of characters in \(word) is \(word.characters.count)")
+
+
+
+
+
