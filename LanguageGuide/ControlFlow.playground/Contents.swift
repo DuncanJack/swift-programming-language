7efba5eeb99b01389c0e10776c67be28dadd3feb
= <=>
2016-07-01 17:45:14 +0100

    Control Transfer Statements  - continue, break

--- LanguageGuide/ControlFlow.playground/Contents.swift
+++ LanguageGuide/ControlFlow.playground/Contents.swift
@@ -173,6 +173,56 @@ default:
     print("Not on an axis")
 }
 
+// -------------------------------------------------------------------------------
+// Control Transfer Statements
+// -------------------------------------------------------------------------------
+
+// Continue
+
+let puzzleInput = "great minds think alike"
+var puzzleOutput = ""
+for character in puzzleInput.characters {
+    switch character {
+    case "a", "e", "i", "o", "u", " ":
+        continue
+    default:
+        puzzleOutput.append(character)
+    }
+}
+print(puzzleOutput)
+
+// Break
+
+let numberSymbol: Character = "三"  // Chinese symbol for the number 3
+var possibleIntegerValue: Int?
+switch numberSymbol {
+case "1", "١", "一", "๑":
+    possibleIntegerValue = 1
+case "2", "٢", "二", "๒":
+    possibleIntegerValue = 2
+case "3", "٣", "三", "๓":
+    possibleIntegerValue = 3
+case "4", "٤", "四", "๔":
+    possibleIntegerValue = 4
+default:
+    break
+}
+if let integerValue = possibleIntegerValue {
+    print("The integer value of \(numberSymbol) is \(integerValue).")
+} else {
+    print("An integer value could not be found for \(numberSymbol).")
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
