d1849bdf3fa736b1bc368ba1bb7198b47ce03e0c
= <=>
2016-07-01 16:19:12 +0100

    Control Flow - While Loops

--- LanguageGuide/ControlFlow.playground/Contents.swift
+++ LanguageGuide/ControlFlow.playground/Contents.swift
@@ -28,3 +28,48 @@ for(animalName, legCount) in numberOfLegs {
 // -------------------------------------------------------------------------------
 // While Loops
 // -------------------------------------------------------------------------------
+
+// While
+
+var finalSquare = 25
+var board = [Int](repeating:0, count:finalSquare + 1)
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+var square = 0
+var diceRoll = 0
+
+while square < finalSquare {
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1 }
+    square += diceRoll
+    if square < board.count {
+        square += board[square]
+    }
+}
+print("Game over!")
+
+// Repeat-While
+
+finalSquare = 25
+board = [Int](repeating: 0, count: finalSquare + 1)
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+square = 0
+diceRoll = 0
+
+repeat {
+    square += board[square]
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1 }
+    square += diceRoll
+} while square < finalSquare
+print("Game over!")
+
+
+
+
+
+
+
+
+
