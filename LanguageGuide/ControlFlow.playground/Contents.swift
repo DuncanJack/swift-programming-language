304cb9a322bece71059c11d99d1905f2ff3ec52d
= <=>
2016-07-01 17:22:10 +0100

    Control Flow - Switch

--- LanguageGuide/ControlFlow.playground/Contents.swift
+++ LanguageGuide/ControlFlow.playground/Contents.swift
@@ -65,9 +65,113 @@ repeat {
 } while square < finalSquare
 print("Game over!")
 
+// -------------------------------------------------------------------------------
+// Conditional Statements
+// -------------------------------------------------------------------------------
 
+// If
 
+var temperatureInFahrenheit = 30
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm.")
+} else {
+    print("It's not that cold.")
+}
 
+// Switch
+
+let someCharacter: Character = "z"
+switch someCharacter {
+case "a":
+    print("The first letter of the alphabet")
+case "z":
+    print("The last letter of the alphabet")
+default:
+    print("Some other character")
+}
+
+// Switch - No Implicit Fallthrough
+
+let anotherCharacter: Character = "a"
+switch anotherCharacter {
+case "a", "A":
+    print("The letter A")
+default:
+    print("Not the letter A")
+}
+
+// Switch - Interval Matching
+
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+var naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+
+// Switch - Tuples
+
+let somePoint = (1,1)
+switch somePoint {
+case (0,0):
+    print("(0, 0) is at the origin")
+case (_,0):
+    print("(\(somePoint.0), 0) is on the x-axis")
+case (0,_):
+    print("(0, \(somePoint.0)) is on the y-axis")
+case (-2...2,-2...2):
+    print("(\(somePoint.0), \(somePoint.1)) is inside the box")
+default:
+    print("\(somePoint) is outside the box")
+}
+
+// Switch - Value Bindings
+
+let anotherPoint = (2,0)
+switch anotherPoint {
+case (let x, 0):
+    print("on the x-axis with an x value of \(x)")
+case (0, let y):
+    print("on the y-axis with a y value of \(y)")
+case let (x,y):
+    print("somewhere else at (\(x), \(y))")
+}
+
+// Switch - Where
+
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    print("(\(x), \(y)) is just some arbitrary point")
+}
+
+// Switch - Compound Cases
+
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("On an axis, \(distance) from the origin")
+default:
+    print("Not on an axis")
+}
 
 
 
