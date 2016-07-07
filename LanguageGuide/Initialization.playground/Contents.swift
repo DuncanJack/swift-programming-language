08dfd2cf916458795c5c8408bb6ef88dd8fda130
= <=>
2016-07-07 15:43:34 +0100

    Initialization - Automatic Initiializer Inheritance

--- LanguageGuide/Initialization.playground/Contents.swift
+++ LanguageGuide/Initialization.playground/Contents.swift
@@ -162,6 +162,43 @@ print(Rect(center: Point(x:4,y:4), size: Size(width:3,height:3)))
 // Class Inheritance and Initialization
 // -------------------------------------------------------------------------------
 
+// Initializer Inheritance and Overriding
+
+class Vehicle {
+    var numberOfWheels = 0
+    var description: String {
+        return "\(numberOfWheels) wheel(s)"
+    }
+}
+let vehicle = Vehicle()
+print("Vehicle: \(vehicle.description)")
+
+class Bicycle: Vehicle {
+    override init() {
+        super.init()
+        numberOfWheels = 2
+    }
+}
+let bicycle = Bicycle()
+print("Bicycle: \(bicycle.description)")
+
+// Automatic Initializer Inheritance
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
