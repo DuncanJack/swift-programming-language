b40afc482c49c52ca0c76fe3dc9ae60bcd6d0e00
= <=>
2016-07-11 18:39:59 +0100

    Automatic Reference Counting - Strong Reference Cycles Between Class Instances

--- LanguageGuide/AutomaticReferenceCounting.playground/Contents.swift
+++ LanguageGuide/AutomaticReferenceCounting.playground/Contents.swift
@@ -1,10 +1,8 @@
 // -------------------------------------------------------------------------------
-// Automatic Reference Counting
+// ARC in Action
 // -------------------------------------------------------------------------------
 
-// ARC in Action
-
-class Person {
+class Personne {
     let name: String
     init(name:String){
         self.name = name
@@ -15,14 +13,44 @@ class Person {
     }
 }
 
-var reference1: Person?
-var reference2: Person?
-var reference3: Person?
+var reference1: Personne?
+var reference2: Personne?
+var reference3: Personne?
 
-reference1 = Person(name:"John Appleseed")
+reference1 = Personne(name:"John Appleseed")
 reference2 = reference1
 reference3 = reference2
 
 reference1 = nil
 reference2 = nil
 reference3 = nil
+
+// -------------------------------------------------------------------------------
+// Strong Reference Cycles Between Class Instances
+// -------------------------------------------------------------------------------
+
+class Person {
+    let name: String
+    init(name: String) { self.name = name }
+    var apartment: Apartment?
+    deinit { print("\(name) is being deinitialized") }
+}
+
+class Apartment {
+    let unit: String
+    init(unit: String) {self.unit = unit }
+    var tenant: Person?
+    deinit { print("Apartment \(unit) is being deinitialized") }
+}
+
+var john: Person?
+var unit4A: Apartment?
+
+john = Person(name: "John Appleseed")
+unit4A = Apartment(unit: "4A")
+
+// This would leave each object with a strong reference to the other
+// john!.apartment = unit4A
+// unit4A!.tenant = john
+john = nil
+unit4A = nil
