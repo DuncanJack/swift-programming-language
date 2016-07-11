981050ca83d34b28ca9081f1c0853cca1c7d3590
= <=>
2016-07-11 18:14:58 +0100

    Automatic Reference Counting - ARC in Action

---
+++ LanguageGuide/AutomaticReferenceCounting.playground/Contents.swift
@@ -0,0 +1,28 @@
+// -------------------------------------------------------------------------------
+// Automatic Reference Counting
+// -------------------------------------------------------------------------------
+
+// ARC in Action
+
+class Person {
+    let name: String
+    init(name:String){
+        self.name = name
+        print("\(name) is being initialized")
+    }
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+var reference1: Person?
+var reference2: Person?
+var reference3: Person?
+
+reference1 = Person(name:"John Appleseed")
+reference2 = reference1
+reference3 = reference2
+
+reference1 = nil
+reference2 = nil
+reference3 = nil
