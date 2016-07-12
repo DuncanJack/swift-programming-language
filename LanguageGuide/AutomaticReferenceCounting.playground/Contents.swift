2bdf943807ad2a23c205990c2f930e51b5b8ba92
= <=>
2016-07-12 17:15:55 +0100

    Automatic Reference Counting - Strong Reference Cycles for Closures

--- LanguageGuide/AutomaticReferenceCounting.playground/Contents.swift
+++ LanguageGuide/AutomaticReferenceCounting.playground/Contents.swift
@@ -109,6 +109,40 @@ class City {
 var country = Country(name: "Canada", capitalName: "Ottowa")
 print("\(country.name)'s capital city is called \(country.capitalCity.name)")
 
+// -------------------------------------------------------------------------------
+// Strong Reference Cycles for Closures
+// -------------------------------------------------------------------------------
+
+class HTMLElement {
+    let name: String
+    let text: String?
+    lazy var asHTML: () -> String = {
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+// A strong reference cycle exists between the instance and the closure (also an instance)
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello world")
+print(paragraph!.asHTML())
+paragraph = nil
+
+
+
+
+
+
+
 
 
 
