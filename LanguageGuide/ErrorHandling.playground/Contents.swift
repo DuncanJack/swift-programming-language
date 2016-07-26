abdcafa929b1c986e7418544b045aee22c223fab
= <=>
2016-07-26 06:47:04 +0100

    Error Handling - Handling Errors (cont'd)

--- LanguageGuide/ErrorHandling.playground/Contents.swift
+++ LanguageGuide/ErrorHandling.playground/Contents.swift
@@ -75,9 +75,21 @@ struct PurchasedSnack {
     }
 }
 
+// Handling Errors Using Do-Catch
 
+var vendingMachine = VendingMachine()
+vendingMachine.coinsDeposited = 8
+do {
+    try buyFavoriteSnack(person: "Alice", vendingMachine: vendingMachine)
+} catch VendingMachineError.invalidSelection {
+    print("Invalid Selection.")
+} catch VendingMachineError.outOfStock {
+    print("Out of Stock.")
+} catch VendingMachineError.insufficientFunds(let coinsNeeded) {
+    print("Insufficient funds, Please insert an additional \(coinsNeeded) coins.")
+}
 
-
+// Converting Errors to Optional Values
 
 
 
