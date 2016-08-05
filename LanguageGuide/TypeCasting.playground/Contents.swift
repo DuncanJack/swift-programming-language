6965d923c64e17d3cc9ee1a222555956f3cd9e36
= <=>
2016-08-05 07:16:58 +0100

    Type Casting - AnyObject

--- LanguageGuide/TypeCasting.playground/Contents.swift
+++ LanguageGuide/TypeCasting.playground/Contents.swift
@@ -53,3 +53,41 @@ print("Media Library contains \(movieCount) movies and \(songCount) songs")
 // -------------------------------------------------------------------------------
 // Downcasting
 // -------------------------------------------------------------------------------
+
+for item in library {
+    if let movie = item as? Movie {
+        print("Movie: \(movie.name), dir. \(movie.director)")
+    } else if let song = item as? Song {
+        print("Song: \(song.name), \(song.name)")
+    }
+}
+
+// -------------------------------------------------------------------------------
+// Type Casting for AnyObject and Any
+// -------------------------------------------------------------------------------
+
+// AnyObject
+
+let someObjects: [AnyObject] = [
+    Movie(name: "2001: A Space Odyssey", director: "Stanley Kubrick"),
+    Movie(name: "Moon", director:"Duncan Jones"),
+    Movie(name: "Aliens", director: "Ridley Scott")
+]
+
+for object in someObjects {
+    let movie = object as! Movie
+    print("Movie: \(movie.name), dir. \(movie.director)")
+}
+
+for movie in someObjects as! [Movie] {
+    print("Movie: \(movie.name), dir. \(movie.director)")
+}
+
+
+
+
+
+
+
+
+
